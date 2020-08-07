4768b87d71b48f5ce14f88246d94a607176ecd86
Dannon Baker <dannon.baker@gmail.com>
2020-08-07 16:41:18 -0400

    Refactoring, split out navbar.

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,6 @@
 <template>
     <div id="app">
-        <header>
-            <nav class="navbar is-primary is-fixed-top" role="navigation" aria-label="main navigation">
-                <div class="container">
-                    <div class="navbar" id="navMenu">
-                        <b-navbar toggleable="lg" type="dark" variant="primary">
-                            <b-navbar-brand to="/">GalaxyClient</b-navbar-brand>
-
-                            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-                            <b-collapse id="nav-collapse" is-nav>
-                                <b-navbar-nav class="ml-auto">
-                                    <b-nav-item-dropdown v-if="this.$store.state.isAuthenticated" right>
-                                        <template v-slot:button-content>
-                                            <em>{{ userEmail }}</em>
-                                        </template>
-                                        <b-dropdown-item href="#">Profile</b-dropdown-item>
-                                        <b-dropdown-item to="/logout">Log Out</b-dropdown-item>
-                                    </b-nav-item-dropdown>
-                                    <b-nav-item v-else to="/login">Log In</b-nav-item>
-                                </b-navbar-nav>
-                            </b-collapse>
-                        </b-navbar>
-                    </div>
-                </div>
-            </nav>
-        </header>
+        <navbar />
         <div class="content-body container">
             <router-view />
         </div>
@@ -33,11 +8,10 @@
     </div>
 </template>
 <script>
+import Navbar from "@/components/Navbar.vue";
 export default {
-    computed: {
-        userEmail() {
-            return this.$store.state.user?.email || null;
-        },
+    components: {
+        Navbar,
     },
 };
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+    <header>
+        <nav class="navbar is-primary is-fixed-top" role="navigation" aria-label="main navigation">
+            <div class="container">
+                <div class="navbar" id="navMenu">
+                    <b-navbar toggleable="lg" type="dark" variant="primary">
+                        <b-navbar-brand to="/">GalaxyClient</b-navbar-brand>
+
+                        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+                        <b-collapse id="nav-collapse" is-nav>
+                            <b-navbar-nav class="ml-auto">
+                                <b-nav-item-dropdown v-if="this.$store.state.isAuthenticated" right>
+                                    <template v-slot:button-content>
+                                        <em>{{ userEmail }}</em>
+                                    </template>
+                                    <b-dropdown-item href="#">Profile</b-dropdown-item>
+                                    <b-dropdown-item to="/logout">Log Out</b-dropdown-item>
+                                </b-nav-item-dropdown>
+                                <b-nav-item v-else to="/login">Log In</b-nav-item>
+                            </b-navbar-nav>
+                        </b-collapse>
+                    </b-navbar>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
+<script>
+export default {
+    computed: {
+        userEmail() {
+            return this.$store.state.user?.email || null;
+        },
+    },
+};
+</script>
